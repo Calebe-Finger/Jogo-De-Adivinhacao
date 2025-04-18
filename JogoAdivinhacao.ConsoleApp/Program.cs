8a524edb06d615ff3533a6ce86f4f9cfc6c83885
Calebe <calebepfinger@gmail.com>
2025-04-18 10:13:33 -0300

    Implementada versão 1: Entrada e Saída de Dados; Pedir para o usuário digitar um número e Escreve-lo no Console.

--- JogoAdivinhacao.ConsoleApp/Program.cs
+++ JogoAdivinhacao.ConsoleApp/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+        //Versão 1: Estrutura básica e entrada do usuário
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Jogo de Adivinhação: ");
+            Console.WriteLine("-----------------------------");
+
+            //Lógica do Jogo
+            Console.Write("Digite um número para churar: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Você digitou o número: " + numeroDigitado);
+
+            Console.ReadLine();
         }
     }
 }
