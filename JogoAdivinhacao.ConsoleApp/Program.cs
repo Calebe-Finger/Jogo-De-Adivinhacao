1092588dc61d73bfd758bf899403950cfec4d914
Calebe <calebepfinger@gmail.com>
2025-04-18 10:37:05 -0300

    Implementação da Versão 2: *Gerar um número aleatório *Comparar o número infomado pelo usuário ao número secreto e exibir se ele é maior, menor, ou igual ao número secreto.

--- JogoAdivinhacao.ConsoleApp/Program.cs
+++ JogoAdivinhacao.ConsoleApp/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        //Versão 1: Estrutura básica e entrada do usuário
+        //Versão 2: Gerar um número secreto aleatório
 
         static void Main(string[] args)
         {
@@ -11,10 +11,28 @@
             Console.WriteLine("-----------------------------");
 
             //Lógica do Jogo
+
+            Random geradorNumeros = new Random();
+
+            int numeroSecreto = geradorNumeros.Next(1, 21);
+
             Console.Write("Digite um número para churar: ");
             int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Você digitou o número: " + numeroDigitado);
+            if (numeroDigitado > numeroSecreto) 
+            {
+                Console.WriteLine("O número digitado é maior que o número secreto.");
+            }
+
+            else if (numeroDigitado < numeroSecreto)
+            {
+                Console.WriteLine("O número digitado é menor que o número secreto.");
+            }
+
+            else
+            {
+                Console.WriteLine("Parabéns, você acertou o número secreto!");
+            }
 
             Console.ReadLine();
         }
